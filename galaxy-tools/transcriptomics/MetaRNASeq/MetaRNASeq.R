7cfbc0f0afdd0b6ccb9b1114cfc8cb5a3869b162
Samuel Blanck <samuel.blanck@univ-lille2.fr>
2016-03-31 09:37:10 +0200

    Correction MetaRNASeq

--- galaxy-tools/transcriptomics/MetaRNASeq/MetaRNASeq.R
+++ galaxy-tools/transcriptomics/MetaRNASeq/MetaRNASeq.R
@@ -9,35 +9,41 @@
 
 cargs <- commandArgs()
 cargs <- cargs[(which(cargs == "--args")+1):length(cargs)]
+nbargs=length(cargs)
 listfiles=vector()
-for (i in 1:length(cargs)-9) {
-	listfiles=c(listfiles,cargs[i])
+listfilenames=vector()
+for (i in seq(1,nbargs-9,2)) {
+	listfiles=c(listfiles,cargs[[i]])
+	listfilenames=c(listfilenames,cargs[[i+1]])
 }
 #countfolder <- cargs[length(cargs) - 7]
-comparison <- unlist(strsplit(cargs[length(cargs) - 8], ','))
-method=cargs[length(cargs) - 7]
-mod<-cargs[length(cargs) - 6]
-outputfile <- cargs[length(cargs) - 5]
-diag.html = cargs[length(cargs) - 4]
-html.files.path=cargs[length(cargs) - 3]
-template=cargs[length(cargs) - 2]
+comparison <- unlist(strsplit(cargs[[length(cargs) - 8]], ','))
+method=cargs[[length(cargs) - 7]]
+mod<-cargs[[length(cargs) - 6]]
+outputfile <- cargs[[length(cargs) - 5]]
+diag.html = cargs[[length(cargs) - 4]]
+html.files.path=cargs[[length(cargs) - 3]]
+template=cargs[[length(cargs) - 2]]
 
 alpha=0.05
 
+print(listfiles)
+print(listfilenames)
+print(comparison)
+print(template)
 
 
 # the comparison must only have two values and the conds must
 # be a vector from those values, at least one of each.
 
 if (length(comparison) != 2) {
-	stop("Comparison type must be a tuple: ", cargs[2])
+	stop("Comparison type must be a tuple: ", cargs[length(cargs) - 8])
 }
 
 sink("/dev/null")
 dir.create(html.files.path, recursive=TRUE)
 library(DESeq)
 library(HTSFilter)
-
 rawpval=list()
 DE=list()
 FC=list()
@@ -46,12 +52,15 @@ i=1
 # Open the html output file
 file.conn = file(diag.html, open="w")
 
-writeLines( c("<html><body bgcolor='lightgray'>"), file.conn)
+writeLines( c("<html><body>"), file.conn)
 
 # Perform deseq analysis on each study
-for(f in listfiles)
+for(i in 1:length(listfiles))
 {
-  study_name=unlist(strsplit(unlist(strsplit(f,"_"))[3],"[.]"))[1]
+  f=listfiles[i]
+  fname=listfilenames[i]
+  study_name=unlist(strsplit(fname,"[.]"))[1]
+  print(paste0("study.name ",study_name))
   d <- read.table(f, sep=" ", header=TRUE, row.names=1)
   conds<-sapply(strsplit(colnames(d),"[.]"),FUN=function(x) x[1])
   if (length(unique(conds)) != 2) {
@@ -76,22 +85,21 @@ for(f in listfiles)
   }
   
   if (length(conds) == 2) {
-  	cds <- estimateDispersions( cds, method=method, sharingMode=mod )
+  	cds <- estimateDispersions( cds, method=method, sharingMode=mod, fitType="parametric" )
   	norep = TRUE
   }
   
   filter<-HTSFilter(cds, plot=FALSE)
   cds.filter<-filter$filteredData
   on.index<-which(filter$on==1)
-  
+
   res<-as.data.frame(matrix(NA,nrow=nrow(cds),ncol=ncol(cds)))
   nbT <- nbinomTest(cds.filter, comparison[1], comparison[2])
   colnames(res)<-colnames(nbT)
   res[on.index,]<-nbT
-  write.table(res[order(res$padj), ], file=outputfile, quote=FALSE, row.names=FALSE, sep="\t")
+  #write.table(res[order(res$padj), ], file=outputfile, quote=FALSE, row.names=FALSE, sep="\t")
   
-  
-  
+
   temp.pval.plot = file.path( html.files.path, paste("PvalHist",i,".png",sep=""))
   png( temp.pval.plot, width=500, height=500 )
   hist(res$pval, breaks=100, col="skyblue", border="slateblue", main="")
@@ -124,7 +132,6 @@ commonsgnFC<-ifelse(abs(sumsigns)==dim(signsFC)[2],sign(sumsigns),0)
 conflits<-data.frame(DE=DE,FC=FC,signFC=commonsgnFC)
 #write DE outputfile
 write.table(conflits, outputfile)
-
 library(VennDiagram)
 DE_num=apply(DE, 2, FUN=function(x) which(x==1))
 venn.plot<-venn.diagram(x=as.list(DE_num),filename=NULL, col="black", fill=1:length(DE_num)+1,alpha=0.6)
@@ -136,5 +143,5 @@ writeLines( c("<h2>Venn Plot</h2>"), file.conn)
 writeLines( c("<img src='venn.png'><br/><br/>"), file.conn)
 writeLines( c("</body></html>"), file.conn)
 close(file.conn)
-
+print("passe6")
 sink(NULL)
