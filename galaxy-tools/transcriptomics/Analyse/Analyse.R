77e8b4cbb16d7860ab7f46892deac815a3150d5a
Samuel Blanck <samuel.blanck@univ-lille2.fr>
2016-11-10 15:01:23 +0100

    smagexp creation

--- galaxy-tools/transcriptomics/Analyse/Analyse.R
+++ galaxy-tools/transcriptomics/Analyse/Analyse.R
@@ -10,56 +10,83 @@ cargs<-commandArgs()
 cargs<-cargs[(which(cargs=="--args")+1):length(cargs)]
 nbargs=length(cargs)
 
-load(cargs[[nbargs-12]])
-targetFile=cargs[[nbargs-11]]
-condition1Name=cargs[[nbargs-10]]
-condition1=cargs[[nbargs-9]]
-condition2Name=cargs[[nbargs-8]]
-condition2=cargs[[nbargs-7]]
-nbresult=cargs[[nbargs-6]]
-result_export_eset=cargs[[nbargs-5]]
-result=cargs[[nbargs-4]]
-result.path=cargs[[nbargs-3]]
+cargs[[nbargs-13]]
+cargs[nbargs-11]
+cargs[[nbargs-2]]
+
+load(cargs[[nbargs-13]])
+targetFile=cargs[[nbargs-12]]
+condition1Name=cargs[[nbargs-11]]
+condition1=cargs[[nbargs-10]]
+condition2Name=cargs[[nbargs-9]]
+condition2=cargs[[nbargs-8]]
+nbresult=cargs[[nbargs-7]]
+result_export_eset=cargs[[nbargs-6]]
+result=cargs[[nbargs-5]]
+result.path=cargs[[nbargs-4]]
+result.tabular=cargs[[nbargs-3]]
 result.template=cargs[[nbargs-2]]
 
-file.copy(targetFile,"./targetFile.txt")
+result.template
+result.tabular
+
+#file.copy(targetFile,"./targetFile.txt")
 
 condition1_tmp <- strsplit(condition1,",")
 condition1 <-unlist(condition1_tmp)
 
-
 condition2_tmp <- strsplit(condition2,",")
 condition2<-unlist(condition2_tmp)
-
 
 conditions=c(condition1,condition2)
 
 #nbresult=1000
 dir.create(result.path, showWarnings = TRUE, recursive = FALSE)
 
-targets <- readTargets("targetFile.txt",path=".",sep="\t")
+targets <- read.table(targetFile,sep="\t")
 
 eset=eset[,which(rownames(eset@phenoData@data) %in% conditions)]
-eset@phenoData@data$source_name_ch1=""
+#condition1Name=make.names(condition1Name)
+#condition2Name=make.names(condition2Name)
+#condition1Name=gsub("_","",condition1Name)
+#condition2Name=gsub("_","",condition2Name)
+#condition1Name
+#condition2Name
 
+
+eset@phenoData@data$source_name_ch1=""
 eset@phenoData@data$source_name_ch1[which(rownames(eset@phenoData@data) %in% condition1)]=condition1Name
 eset@phenoData@data$source_name_ch1[which(rownames(eset@phenoData@data) %in% condition2)]=condition2Name
+#condition1Name
+#condition2Name
 
-sml=paste0("G",as.numeric(as.character(pData(eset)["source_name_ch1"][,1])!=condition1Name))
-fl <- as.factor(sml)
-eset$description <- fl
-design <- model.matrix(~ description + 0, eset)
+condNames=paste0("G",as.numeric(as.character(pData(eset)["source_name_ch1"][,1])!=condition1Name))
+#condNames=make.names(targets[,2])
+#condNames=gsub("_","",condNames)
 
+f <- as.factor(condNames)
+f
+#eset$description <- factors
+design <- model.matrix(~ 0+f)
+design
 
-colnames(design) <- levels(fl)
+colnames(design) <- levels(f)
+colnames(design)
 fit <- lmFit(eset, design)
-cont.matrix <- makeContrasts(G1-G0, levels=design)
+fit
+#cont.matrix <- makeContrasts(C1=paste0(condition1Name,"-",condition2Name), levels=design)
+cont.matrix <- makeContrasts(G0-G1, levels=design)
+cont.matrix
 fit2 <- contrasts.fit(fit, cont.matrix)
 fit2 <- eBayes(fit2)
+fit2
 tT <- topTable(fit2, adjust="fdr", sort.by="B", number=nbresult)
+
+#head(exprs(eset))
 
 gpl <- annotation(eset)
 if (substr(x = gpl,1,3)!="GPL"){
+	#if the annotation info does not start with "GPL" we retrieve the correspondin GPL annotation
 	mapping=read.csv("/galaxy-tools/transcriptomics/db/gplToBioc.csv",stringsAsFactors=FALSE)
 	gpl=mapping[which(mapping$bioc_package==annotation(eset)),]$gpl
 	gpl=gpl[1]
@@ -74,20 +101,8 @@ if (substr(x = gpl,1,3)!="GPL"){
 	colnames(fData(eset))[4]="ENTREZ_GENE_ID"
 	row.names(fData(eset))=fData(eset)[,"ID"]
 	
-	# replace original platform annotation
 	tT <- add_rownames(tT, "ID")
-	tT <- merge(tT, ncbifd, by="ID")
-	tT <- tT[order(tT$P.Value), ]  # restore correct order
 	
-	tT <- subset(tT, select=c("Platform_SPOTID","ID","adj.P.Val","P.Value","t","B","logFC","Gene.symbol","Gene.title","Gene.ID","Chromosome.annotation","GO.Function.ID"))
-	tT<-format(tT, digits=2, nsmall=2)
-	colnames(tT)=gsub(pattern = "\\.",replacement = "_",colnames(tT))
-	matrixtT=as.matrix(tT)
-	datajson=toJSON(matrixtT,pretty = TRUE)
-	
-	htmlfile=readChar(result.template, file.info(result.template)$size)
-	htmlfile=gsub(x=htmlfile,pattern = "###DATAJSON###",replacement = datajson, fixed = TRUE)
-	dir.create(result.path, showWarnings = TRUE, recursive = FALSE)
 } else {
 	
 	gpl <- annotation(eset)
@@ -96,19 +111,22 @@ if (substr(x = gpl,1,3)!="GPL"){
 	
 	if (!("ID" %in% colnames(tT))){
 		tT <- add_rownames(tT, "ID")}
-	tT <- merge(tT, ncbifd, by="ID")
-	tT <- tT[order(tT$P.Value), ]  
 	
-	tT <- subset(tT, select=c("Platform_SPOTID","ID","adj.P.Val","P.Value","t","B","logFC","Gene.symbol","Gene.title","Gene.ID","Chromosome.annotation","GO.Function.ID"))
-	tT<-format(tT, digits=2, nsmall=2)
-	colnames(tT)=gsub(pattern = "\\.",replacement = "_",colnames(tT))
-	matrixtT=as.matrix(tT)
-	datajson=toJSON(matrixtT,pretty = TRUE)
-	
-	htmlfile=readChar(result.template, file.info(result.template)$size)
-	htmlfile=gsub(x=htmlfile,pattern = "###DATAJSON###",replacement = datajson, fixed = TRUE)
-	dir.create(result.path, showWarnings = TRUE, recursive = FALSE)
 }
+
+tT <- merge(tT, ncbifd, by="ID")
+tT <- tT[order(tT$P.Value), ] 
+tT <- subset(tT, select=c("Platform_SPOTID","ID","adj.P.Val","P.Value","t","B","logFC","Gene.symbol","Gene.title","Gene.ID","Chromosome.annotation","GO.Function.ID"))
+tT<-format(tT, digits=2, nsmall=2)
+head(tT)
+colnames(tT)=gsub(pattern = "\\.",replacement = "_",colnames(tT))
+matrixtT=as.matrix(tT)
+datajson=toJSON(matrixtT,pretty = TRUE)
+
+htmlfile=readChar(result.template, file.info(result.template)$size)
+htmlfile=gsub(x=htmlfile,pattern = "###DATAJSON###",replacement = datajson, fixed = TRUE)
+dir.create(result.path, showWarnings = TRUE, recursive = FALSE)
+
 boxplot="boxplot.png"
 png(boxplot,width=800,height = 400)
 par(mar=c(7,5,1,1))
@@ -127,10 +145,9 @@ htmlfile=gsub(x=htmlfile,pattern = "###HIST###",replacement = histopvalue, fixed
 dev.off()
 file.copy(histopvalue,result.path)
 
-
-
 #write.table(tolower(c(condition1Name,condition2Name)),quote = FALSE,col.names = FALSE, row.names=FALSE,file=result_export_conditions)
 saveConditions=c(condition1Name,condition2Name)
 save(eset,saveConditions,file=result_export_eset)
+write.table(x=tT[,-1],file=result.tabular,quote=FALSE,row.names=FALSE,col.names=TRUE,sep="\t")
 write(htmlfile,result)
 
--- galaxy-tools/transcriptomics/GEOquery/GEOQuery.R
+++ galaxy-tools/transcriptomics/GEOquery/GEOQuery.R
@@ -1,5 +1,4 @@
 
-#---- lib ---------------
 library(GEOquery)
 
 
@@ -15,6 +14,7 @@ transformation<-cargs[[5]]
 data1 = getGEO(GEOQueryID)
 eset=data1[[1]]
 
+#check if datas are in log2 space
 normalization<-function(data){
 	ex <- exprs(data)
 	qx <- as.numeric(quantile(ex, c(0., 0.25, 0.5, 0.75, 0.99, 1.0), na.rm=T))
@@ -35,20 +35,22 @@ if (transformation=="auto"){
 
 matrixData=exprs(eset)
 write.table(matrixData,col.names=NA,row.names=TRUE,sep="\t",file=GEOQueryData)
+
+#Construcion of condition file
+#if there is data in "source_name_ch1" field, we keep this data as a condition
+#else we keep the "description" field data.
 if (length(unique(tolower(pData(data1[[1]])["source_name_ch1"][,1])))>1)
 {
 	conditions=pData(data1[[1]])["source_name_ch1"]
 	description=paste0(as.vector(pData(data1[[1]])["geo_accession"][,1]), " ",as.vector(pData(data1[[1]])["title"][,1]), " ", as.vector(conditions[,1]))
-	#file=rownames(pData(data1[[1]]))
 }	else
 {
 	conditions=pData(data1[[1]])["description"]
 	description=paste0(as.vector(pData(data1[[1]])["geo_accession"][,1]), " ",as.vector(pData(data1[[1]])["title"][,1]), " ", as.vector(conditions[,1]))
-	#file=rownames(pData(data1[[1]]))
 }
+
 conditions[,1]=tolower(conditions[,1])
 pData(eset)["source_name_ch1"]=conditions
 
-#write.table(unique(tolower(conditions[,1])),quote = FALSE,col.names = FALSE, row.names=FALSE,file=conditionFile)
 write.table(cbind(conditions,description),quote = FALSE,col.names = FALSE, row.names=TRUE,file=conditionFile,sep="\t")
 save(eset,conditions,file=GEOQueryRData)
